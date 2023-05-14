e45e22470da4e3e710d72e6123894314368b7955
Latif Alidu <latif.alidu@agiliacenter.com>
2023-05-14 17:36:44 +0000

    access origin set

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const app = require("express")();
 const httpServer = require("http").createServer(app);
-const origin = "https://funconnect.net";
+const origin = process.env.NODE_ENV
+  ? "https://funconnect.net"
+  : "http://localhost:3000";
 
-//const origin =  "http://localhost:3000";;
-console.log(process.env.NODE_ENV);
-console.log(origin);
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: origin,
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
